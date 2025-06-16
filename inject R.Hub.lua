700ad7dd06187f17868718306258e60ae3a1f290
Dkzin.rlk <79470887+SrDark222@users.noreply.github.com>
2025-06-16 02:50:52 -0300

    Update and rename inject R.Hub.lua.txt to inject R.Hub.lua

--- inject R.Hub.lua
+++ inject R.Hub.lua
@@ -17,7 +17,7 @@ local redzlib = {
 				ColorSequenceKeypoint.new(1.00, Color3.fromRGB(5, 0, 50))
 			}),
 			["Color Hub 2"] = Color3.fromRGB(0, 0, 0),
-			["Color Stroke"] = Color3.fromRGB(5, 5, 5),
+			["Color Stroke"] = Color3.fromRGB(255, 5, 5),
 			["Color Theme"] = Color3.fromRGB(880, 0, 0),
 			["Color Text"] = Color3.fromRGB(243, 243, 243),
 			["Color Dark Text"] = Color3.fromRGB(180, 180, 180)
